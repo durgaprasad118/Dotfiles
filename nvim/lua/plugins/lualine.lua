3136df65a4fbb36f8984dc82d03f27f0c6a951a7
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-07-22 00:03:53 +0530

    Updated neovim lualine veiw

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,35 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "folke/noice.nvim",
+  },
   event = "VeryLazy",
   opts = {
     options = {
-      -- theme = "catapuccin",
+      theme = "catppuccin",
+      icons_enabled = true,
+      component_separators = "|",
+      section_separators = "",
+    },
+    sections = {
+      lualine_a = {
+        {
+          "buffers",
+        },
+      },
+      lualine_x = {
+        {
+          require("noice").api.statusline.mode.get,
+          cond = require("noice").api.statusline.mode.has,
+          color = { fg = "#ff9e64" },
+        },
+        {
+          require("noice").api.status.command.get,
+          cond = require("noice").api.status.command.has,
+          color = { fg = "#ff9e64" },
+        },
+      },
     },
   },
 }
